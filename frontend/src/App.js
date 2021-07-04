8b9444c36c75b4d6ece5117afa65fe5af61a9f7c
sidhant <sidhant1593@gmail.com>
2021-07-04 23:30:55 +0530

    product frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
@@ -5,11 +8,15 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <Router>
+      <React.Fragment>
+        <Header />
+        <div className="container container-fluid">
+          <Route path="/" component={Home} exact />
+        </div>
+        <Footer />
+      </React.Fragment>
+    </Router>
   );
 }
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,41 +1,42 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { Fragment, useEffect } from "react";
+import Metadata from "./layout/Metadata";
+
+import Product from "./product/Product";
+import Loader from "./layout/Loader";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../actions/productActions";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const { loading, products, error, productsCount } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
   return (
-    <div className="container container-fluid">
-      <h1 id="products_heading">Latest Products</h1>
-      <section id="products" className="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-                alt="sss"
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                  <a href="#">
-                    128GB Solid Storage Memory card - SanDisk Ultra
-                  </a>
-                </h5>
-                <div className="ratings mt-auto">
-                  <div className="rating-outer">
-                    <div className="rating-inner"></div>
-                  </div>
-                  <span id="no_of_reviews">(5 Reviews)</span>
-                </div>
-                <p className="card-text">Rs 45.67</p>
-                <a href="#" id="view_btn" className="btn btn-block">
-                  View Details
-                </a>
-              </div>
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <Metadata title={`Buy Best Products Online`} />
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
             </div>
-          </div>
-        </div>
-      </section>
-    </div>
+          </section>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
